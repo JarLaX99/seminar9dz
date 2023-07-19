8017d332cbeeac88a53770d87b9c008999ac9183
JarLaX99 <vanek10699@gmail.com>
2023-07-19 09:35:17 +0300

    Семинар 9, задание 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void main()
+{
+    int numberA = Readint("Введите первое число : ");
+    int numberB = Readint("Введите второе  число: ");
+    int sum = 0;
+    System.Console.WriteLine($"сумма чисел от {numberA} до {numberB} = {SumNumbers(numberA, numberB, sum)}");
+}
+
+int SumNumbers(int numberA, int numberB, int sum)
+{
+    if (numberA > numberB) return 0;
+
+    return sum += numberA + SumNumbers(numberA + 1, numberB, sum);
+}
+
+int Readint(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+main();
