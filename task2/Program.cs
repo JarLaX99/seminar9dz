ecf647acd42b32224ff87a7ece115ddf9114fd1b
JarLaX99 <vanek10699@gmail.com>
2023-07-19 09:52:21 +0300

    Семинар 9, задание 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+void main()
+{
+    int numberA = Readint("Введите первое число : ");
+    int numberB = Readint("Введите второе  число: ");
+    System.Console.WriteLine(A(numberA, numberB));
+}
+
+int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0 && n != 0) return A(n - 1, 1);
+    else return A(n - 1, A(n, m- 1));
+
+}
+
+
+
+int Readint(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+main();
